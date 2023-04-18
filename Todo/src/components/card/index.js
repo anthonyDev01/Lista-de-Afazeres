47e993aef73f647247de0de630f7e6f7a20516c8
ANTHONY <anthonychukwudiDev@outlook.com>
2023-04-17 22:01:10 -0300

    primeiro projeto utilizando ReactJs

---
+++ Todo/src/components/card/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Card(props){
+    return(
+        <div className="card">
+            {props.children}
+        </div>
+    )
+}
+
+"primeiro projeto utilizando ReactJs"
